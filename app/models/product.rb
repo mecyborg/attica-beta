42ce611e07a2327c9491178a3f2321732e862913
shantanu21 <shantanu217@gmail.com>
2015-04-04 22:48:41 +0530

    First Commit

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,9 @@
 class Product < ActiveRecord::Base
+	has_many :order_items
 	has_attached_file :avatar, :styles => {:large => "350x350>", :medium => "200x200>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
   	belongs_to :catgs
   	validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
   	validates_presence_of :name, :price, :discount, :description, :category,:instock,:avatar
 
+  	# default_scope { where(active: true) }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,18 @@
 Rails.application.routes.draw do
+  get 'order_items/create'
+
+  get 'order_items/update'
+
+  get 'order_items/destroy'
+
+  get 'carts/show'
+
   devise_for :users
   resources :products
 
   resources :catgs
-
+  resource :cart, only: [:show]
+  resources :order_items, only: [:create, :update, :destroy]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
